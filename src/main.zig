96b76951d64d63f385366751e71b480625da5114
znichola <niki.zivkovic@hotmail.com>
2025-01-15 10:59:31 +0100

    movable character

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,26 @@
 const std = @import("std");
 const rl = @import("raylib");
 
+// pub const OrbitalEntity = struct {};
+
+pub const GameState = struct {
+    pos: rl.Vector2,
+
+    pub fn toScreenX(self: GameState) i32 {
+        return @as(i32, @intFromFloat(self.pos.x));
+    }
+    pub fn toScreenY(self: GameState) i32 {
+        return @as(i32, @intFromFloat(self.pos.y));
+    }
+};
+
 pub fn main() anyerror!void {
+    //Game Variables (MODLE)
+    //--------------------------------------------------------------------------------------
+    var gameState = GameState{ .pos = rl.Vector2.init(100, 100) };
+
+    //-end MODLE----------------------------------------------------------------------------
+
     // Initialization
     //--------------------------------------------------------------------------------------
     const screenWidth = 800;
@@ -15,19 +34,28 @@ pub fn main() anyerror!void {
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Update
+        // (Update)
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        var tmpPos = rl.Vector2.init(0, 0);
+        if (rl.isKeyDown(rl.KeyboardKey.w)) tmpPos = tmpPos.add(rl.Vector2.init(0.0, -1.0));
+        if (rl.isKeyDown(rl.KeyboardKey.s)) tmpPos = tmpPos.add(rl.Vector2.init(0.0, 1.0));
+        if (rl.isKeyDown(rl.KeyboardKey.a)) tmpPos = tmpPos.add(rl.Vector2.init(-1.0, 0.0));
+        if (rl.isKeyDown(rl.KeyboardKey.d)) tmpPos = tmpPos.add(rl.Vector2.init(1.0, 0.0));
+        tmpPos = tmpPos.normalize().scale(2.0);
+        gameState.pos = gameState.pos.add(tmpPos);
 
-        // Draw
+        // if (rl.isKeyPressed(rl.KeyboardKey.key_up)) options.dt.dtUp();
+        // if (rl.isKeyPressed(rl.KeyboardKey.key_down)) options.dt.dtDown();/
+
+        // Draw (VIEW)
         //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
+        rl.drawFPS(10, 10);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        rl.drawCircle(gameState.toScreenX(), gameState.toScreenY(), 4, rl.Color.red);
         //----------------------------------------------------------------------------------
     }
 }
